6d5a889790cee439c51a339b7c0f458173127e35
Yudhvir Singh <yudhvirs01@gmail.com>
2020-12-29 10:59:57 -0800

    added cards object

---
+++ src/cards.py
@@ -0,0 +1,28 @@
+import random
+
+# GLOBALS
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+card_dict = {'Two':2,
+        'Three':3,
+        'Four':4,
+        'Five':5, 
+        'Six':6,
+        'Seven':7,
+        'Eight':8,
+        'Nine':9,
+        'Ten':10,
+        'Jack':10,
+        'Queen':10, 
+        'King':10, 
+        'Ace':11}
+
+class Card:
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = card_dict[rank]
+
+    def __str__(self):
+        return self.rank + " of " + self.rank
