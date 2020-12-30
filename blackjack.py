43c7a1662e0f3b0b67f5664a420423b9b2cbdb30
Yudhvir Singh <yudhvirs01@gmail.com>
2020-12-29 21:19:42 -0800

    Blackjack game logic

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,64 @@
-from src import Card, Deck
+from src import Card, Deck, Hand, Chips
+from utils import *
 
-new_deck = Deck()
+playing = True
 
-print(new_deck)
+while True:
+
+    print("BLACKJACK")
+
+    deck = Deck()
+    deck.shuffle()
+
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+
+    player_chips = Chips()
+    take_bet(player_chips)
+
+    show_some(player_hand, dealer_hand)
+
+    while playing:
+        playing = hit_or_stand(deck, player_hand, playing)
+
+        show_some(player_hand, dealer_hand)
+
+        if player_hand.value > 21:
+            player_busts(player_hand, dealer_hand, player_chips)
+            break
+
+    if player_hand.value <=21:
+
+        while dealer_hand.value < 17:
+            hit(deck, dealer_hand)
+
+        show_all(player_hand, dealer_hand)
+
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand, dealer_hand, player_chips)
+
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand, dealer_hand, player_chips)
+
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand, dealer_hand, player_chips)
+
+        else:
+            push(player_hand, dealer_hand)
+
+    print(f'\nPlayer total chips are at: {player_chips.total}')
+
+    new_game = input("Do you want to play again? 'Y' or 'N': ")
+
+    if new_game[0].lower() == 'y':
+        playing = True
+        continue
+
+    else:
+        print('Thank you for playing')
+        break
